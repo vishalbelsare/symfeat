e78b21e49761be87e27c3da8529848c625bbe99d
mq <mquade@uni-potsdam.de>
2017-01-24 12:15:51 -0800

    init

---
+++ Makefile
@@ -0,0 +1,9 @@
+run-tests:
+	py.test tests/
+
+init:
+	pip install -r requirements.txt
+	pip install -e .
+
+dev: init
+	pip install py.test
---
+++ setup.py
@@ -0,0 +1,41 @@
+import os
+import sys
+from os.path import dirname
+
+from setuptools import setup
+
+here = os.path.abspath(dirname(__file__))
+
+with open(os.path.join(here, 'README.md')) as f:
+    long_description = '\n' + f.read()
+
+base_dir = os.path.dirname(__file__)
+
+about = {}
+with open(os.path.join(base_dir, "symfeat", "__version__.py")) as f:
+    exec(f.read(), about)
+
+if sys.argv[-1] == "publish":
+    os.system("python setup.py sdist bdist_wheel upload")
+    sys.exit()
+
+required = [
+]
+
+setup(
+    name='symfeat',
+    version=about['__version__'],
+    description='Ruled based feature engineering for regression',
+    long_description=long_description,
+    author='Markus Quade',
+    author_email='info@markusqua.de',
+    url='https://github.com/ohjeah/symfeat',
+    packages=['symfeat'],
+    install_requires=required,
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
